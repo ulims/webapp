df4b5b0a9c849ba0ef04d748c795ea132bb3bd2a
Agbama Ulimhuka <ulimhukaakem@gmail.com>
2024-12-06 22:04:21 +0100

    test

--- app.vue
+++ app.vue
@@ -4,23 +4,3 @@
     <NuxtWelcome />
   </div>
 </template>
-<script setup>
-useHead({
-  meta: [
-    { 
-      name: 'apple-itunes-app', 
-      content: `app-id=com.example.customer, app-argument=https://trychow.vercel.app/restaurant/${restaurantId}`
-    },
-    {
-      name: 'google-play-app',
-      content: 'app-id=com.example.customer'
-    }
-  ],
-  link: [
-    {
-      rel: 'alternate',
-      href: `android-app://com.example.customer/https/trychow.vercel.app/restaurant/${restaurantId}`
-    }
-  ]
-})
-</script>
